eae636f20fe7295d05e513006e7a1c3449adb3c4
Jason Li <lizhansong@hotmail.com>
2013-10-19 22:59:56 +0800

    modified:   90_94.hs 	modified:   95_99.hs

--- 90_94.hs
+++ 90_94.hs
@@ -1,1 +1,33 @@
---placeholder
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+queens :: Int -> [[Int]]
+queens 0 = []
+queens 1 = [[1]]
+queens n = iter 1 Set.empty Set.empty Set.empty []
+    where check x y vrow vv1 vv2 = not (Set.member y vrow ||
+                                        Set.member (x-y) vv1 ||
+                                        Set.member (x+y) vv2)
+
+          iter x vrow vv1 vv2 cur
+            | x == (n+1) = [cur]
+            | otherwise = let ys = filter (\y -> check x y vrow vv1 vv2) [1..n] in
+                            concat $ map (\y -> let vrow' = Set.insert y vrow
+                                                    vv1' = Set.insert (x-y) vv1
+                                                    vv2' = Set.insert (x+y) vv2 in
+                                                  iter (x+1) vrow' vv1' vv2' (cur++[y])) ys
+
+
+
+knightsTo :: Int -> (Int,Int) -> [[(Int,Int)]]
+knightsTo n (x0,y0) = iter (x0,y0) [(x0,y0)] $ Set.singleton (x0,y0)
+    where deltas = [(1,2),(1,-2),(-1,2),(-1,-2),
+                    (2,1),(2,-1),(-2,1),(-2,-1)]
+
+          iter (cx,cy) path vs
+            | (Set.size vs) == (n*n) = [reverse path]
+            | otherwise = let next = map (\(dx,dy) -> (cx+dx,cy+dy)) deltas
+                              next' = filter (\(nx,ny) ->
+                                                    (nx > 0) && (ny > 0) && (nx <= n) && (ny <= n) && not (Set.member (nx,ny) vs)) next in
+                            concat $ map (\(nx,ny) -> iter (nx,ny) ((nx,ny):path) (Set.insert (nx,ny) vs)) next'
+
--- 95_99.hs
+++ 95_99.hs
@@ -1,1 +1,26 @@
---placeholder
+import Data.List
+
+fullWords :: Int -> String
+fullWords n = let sn = show n in
+                foldr1 step $ map (\sx -> digits !! (read [sx])) (show n)
+    where step s1 s2 = s1 ++ "-" ++ s2
+          digits = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+
+identifier :: String -> Bool
+identifier s = letters1 s
+    where hyphen ('-':s) = letters1 s
+          hyphen _ = False
+
+          letters "" = True
+          letters (s:ss)
+            | (s `elem` ['a'..'z'] || s `elem` ['A'..'Z']) = letters ss
+            | (s == '-') = hyphen (s:ss)
+            | otherwise = False
+
+          letters1 "" = False
+          letters1 (s:ss)
+            | (s `elem` ['a'..'z'] || s `elem` ['A'..'Z']) = letters ss
+            | otherwise = False
+
+
+
