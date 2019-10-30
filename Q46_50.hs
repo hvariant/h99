d4ee233686add7ebded246fb7e11dee5c83054e4
zhansongl <zhansong.li@ir.com>
2019-10-30 13:46:03 +1100

    add tests for Q46_50

--- Q46_50.hs
+++ Q46_50.hs
@@ -1,4 +1,9 @@
-import Data.List
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
+import Data.List (sortBy)
+
+import Test.Hspec
 
 and' :: Bool -> Bool -> Bool
 or' :: Bool -> Bool -> Bool
@@ -29,12 +34,10 @@ gen_entry 1 = [[True],[False]]
 gen_entry k = let rs' = gen_entry (k-1) in
                 (map (True:) rs') ++ (map (False:) rs')
 
-pairs :: [[Bool]]
-pairs = gen_entry 2
-
 table2 :: (Bool -> Bool -> Bool) -> String
-table2 bf = unlines $ map f pairs
+table2 bf = unlines $ map f (gen_entry 2)
     where f (x:y:[]) = show x ++ " " ++ show y ++ " " ++ show (bf x y)
+          f _ = ""
 
 tablen :: Int -> ([Bool] -> Bool) -> String
 tablen n nf = unlines $ map f (gen_entry n)
@@ -49,11 +52,12 @@ gray n = let r' = gray (n-1) in
 {-huffman :: (Eq a) => [(a,Int)] -> [(a,String)]-}
 data Tree a = Leaf Int a | Node Int (Tree a) (Tree a) deriving (Show)
 
+treeFreq :: Tree a -> Int
 treeFreq (Leaf n _) = n
 treeFreq (Node n _ _) = n
 
+treeComp :: Tree a -> Tree a -> Ordering
 treeComp t1 t2 = compare (treeFreq t1) (treeFreq t2)
-
 
 insertSorted :: (a -> a -> Ordering) -> a -> [a] -> [a]
 insertSorted _ x [] = [x]
@@ -62,6 +66,7 @@ insertSorted f x l@(y:ys) = case f x y of
                                 LT -> (x:l)
                                 GT -> (y:(insertSorted f x ys))
 
+huffman :: [(a, Int)] -> [(a, String)]
 huffman xs = let xs' = sortBy (\(_,f1) -> (\(_,f2) -> compare f1 f2)) xs
                  tree = buildtree (map (\(x,n) -> Leaf n x) xs') in
                  reverse $ traverse tree [] ""
@@ -69,8 +74,32 @@ huffman xs = let xs' = sortBy (\(_,f1) -> (\(_,f2) -> compare f1 f2)) xs
           buildtree (x1:x2:xs) = let f1 = treeFreq x1
                                      f2 = treeFreq x2 in
                                      buildtree $ insertSorted treeComp (Node (f1+f2) x1 x2) xs
+          buildtree [] = undefined
 
           traverse (Leaf _ x) codes path = ((x,path):codes)
           traverse (Node _ t1 t2) codes path = let c1 = traverse t1 codes (path ++ "0") in
                                                 traverse t2 c1 (path ++ "1")
 
+main :: IO ()
+main = hspec $ do
+  describe "table2" $ do
+    it "works" $ do
+      table2 (\a b -> a `and'` (a `or'` not b))
+        `shouldBe` "True True True\nTrue False True\nFalse True False\nFalse False False\n"
+
+  describe "tablen" $ do
+    it "works" $ do
+      tablen 3 (\[a,b,c] -> a `and'` (b `or'` c) `equ'` a `and'` b `or'` a `and'` c)
+        `shouldBe` "True True True True\nTrue True False True\n"
+                ++ "True False True True\nTrue False False True\n"
+                ++ "False True True True\nFalse True False True\n"
+                ++ "False False True True\nFalse False False True\n"
+
+  describe "gray" $ do
+    it "works" $ do
+      gray 3 `shouldBe` ["000","001","011","010","110","111","101","100"]
+
+  describe "huffman" $ do
+    it "works" $ do
+      huffman [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
+        `shouldBe` [('a',"0"),('c',"100"),('b',"101"),('f',"1100"),('e',"1101"),('d',"111")]
